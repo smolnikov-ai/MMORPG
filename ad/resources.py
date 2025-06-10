5d91d36787b53c571ef20225a90e95b414338a25
smolnikov-ai <smolnikov-ai@mail.ru>
2025-06-10 16:16:16 +0300

    change .models, add resources

---
+++ ad/resources.py
@@ -0,0 +1,25 @@
+# choices list for field category model Advertisement
+
+tanks = 'TK'
+healers = "HL"
+damage_dealer = "DD"
+merchants = "MC"
+guild_masters = "GM"
+quest_givers = "QG"
+blacksmiths = "BS"
+tanners = "TN"
+potion_makers = "PM"
+spellmasters = "SM"
+
+ADVERTISEMENT_CATEGORY_CHOICES = [
+    (tanks, "Tanks"),
+    (healers, "Healers"),
+    (damage_dealer, "DD"),
+    (merchants, "Merchants"),
+    (guild_masters, "Guild masters"),
+    (quest_givers, "Quest givers"),
+    (blacksmiths, "Blacksmiths"),
+    (tanners, "Tanners"),
+    (potion_makers, "Potion makers"),
+    (spellmasters, "Spellmasters"),
+]
