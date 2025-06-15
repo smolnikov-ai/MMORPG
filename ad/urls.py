660ccbc4e8470dddb0d4a2f0c3e3e9a89f7d7ef0
smolnikov-ai <smolnikov-ai@mail.ru>
2025-06-15 14:18:59 +0300

    add registration

--- ad/urls.py
+++ ad/urls.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
-from .views import (AdList, AdDetail, AdCreate)
+from .views import (AdList, AdDetail, AdCreate, UserRegisterView, EmailConfirmedView, EmailConfirmationSentView,
+                    UserConfirmEmailView, EmailConfirmationFailedView)
 
 
 urlpatterns = [
@@ -9,4 +11,11 @@ urlpatterns = [
     path('ad/<int:pk>/', AdDetail.as_view(), name='ad-detail'),
     # creating a new advertisement
     path('ad/create/', AdCreate.as_view(), name='ad-create'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('email-confirmation-sent/', EmailConfirmationSentView.as_view(), name='email-confirmation-sent'),
+    path('email-confirmed/', EmailConfirmedView.as_view(), name='email-confirmed'),
+    path('confirm-email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm-email'),
+    path('confirm-email-failed/', EmailConfirmationFailedView.as_view(), name='email_confirmation_failed'),
+
 ]
